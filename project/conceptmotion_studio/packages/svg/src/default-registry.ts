7619d2ea21bd5cd1ab716d5bd3d79d653cf63197
Julian Passebecq <64646018+julian-passebecq@users.noreply.github.com>
2026-09-07 01:03:02 +0200

    experiment: compose collection and Table Trace renderer registries

--- project/conceptmotion_studio/packages/svg/src/default-registry.ts
+++ project/conceptmotion_studio/packages/svg/src/default-registry.ts
@@ -1,15 +1,18 @@
 import { createRendererRegistry, type RendererRegistry } from './registry.js';
+import { registerCollectionRenderers } from './renderers/collection.js';
 import { registerDiagramRenderers } from './renderers/diagram.js';
 import { registerJoinRenderers } from './renderers/join.js';
 import { registerLineageRenderers } from './renderers/lineage.js';
 import { registerLoopRenderers } from './renderers/loop.js';
 import { registerRegressionRenderers } from './renderers/regression.js';
 import { registerTableRenderers } from './renderers/table.js';
+import { registerTableTraceRenderers } from './renderers/trace.js';
 import { registerWorkflowRenderers } from './renderers/workflow.js';
 
 export function registerDefaultRendererFamilies(registry: RendererRegistry): RendererRegistry {
   registerCollectionRenderers(registry);
   registerTableRenderers(registry);
+  registerTableTraceRenderers(registry);
   registerJoinRenderers(registry);
   registerLoopRenderers(registry);
   registerRegressionRenderers(registry);
@@ -22,4 +25,3 @@ export function registerDefaultRendererFamilies(registry: RendererRegistry): Ren
 export function createDefaultRendererRegistry(): RendererRegistry {
   return registerDefaultRendererFamilies(createRendererRegistry());
 }
-import { registerCollectionRenderers } from './renderers/collection.js';
