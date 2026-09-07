a470441f7324c0b78ed56c28b6d3388bfedc0f46
Julian Passebecq <64646018+julian-passebecq@users.noreply.github.com>
2026-09-07 08:33:19 +0200

    test(figure): include Table Trace in default adapter registry

--- project/conceptmotion_studio/packages/figure/tests/registry.test.tsx
+++ project/conceptmotion_studio/packages/figure/tests/registry.test.tsx
@@ -39,6 +39,7 @@ describe('FigureRendererRegistry', () => {
       'static.text',
       'statistics.regression',
       'table.join',
+      'table.trace',
       'table.transform',
       'workflow.run',
       'workflow.topology',
